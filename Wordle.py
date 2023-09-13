a39cc0a813c80b0a0cb9104d55c86faf270e4104
tannergwood <tg357@byu.edu>
2023-09-12 20:54:51 -0600

    Most game function

--- Wordle.py
+++ Wordle.py
@@ -11,19 +11,19 @@ import random
 from WordleDictionary import FIVE_LETTER_WORDS
 from WordleGraphics import WordleGWindow, N_COLS, N_ROWS
 
+# MAKE THE GAME END ONCE THERE ARE SIX GUESSES
 def wordle():
-    row = 0
     # SELECT A RANDOM WORD and MAKE IT UPPERCASE
     word = random.choice(FIVE_LETTER_WORDS).upper()
+    print(word)
 
     def enter_action(s):
+
         # RETRIEVE WORD FROM WORDLE
         inputWord = ""
-        i = N_COLS
         for x in range(0, N_COLS):
-            letter = WordleGWindow.get_square_letter(gw, row, N_COLS - i)
+            letter = WordleGWindow.get_square_letter(gw, WordleGWindow.get_current_row(gw), x)
             inputWord = inputWord + letter
-            i = i - 1
 
         #MILESTONE 2 MAKE SURE THE WORD IS AN ENGLISH WORD
         if (inputWord.lower() in FIVE_LETTER_WORDS):
@@ -37,8 +37,14 @@ def wordle():
                 # SELECT NEXT ROWS
                 WordleGWindow.set_current_row(gw,WordleGWindow.get_current_row(gw) + 1)
         else:
-            gw.show_message("Not in word list")
-            # CLEAR THE ROW, REMAIN IN THE SAME ROW
+            if inputWord == "     ":
+                gw.show_message("Enter a word")
+            else:
+                gw.show_message("Not in word list")
+                # CLEAR THE ROW REMAIN IN THE SAME ROW
+                for x in range(0, N_COLS):
+                    WordleGWindow.set_square_letter(gw, WordleGWindow.get_current_row(gw), x, "")
+                WordleGWindow.set_current_row(gw, WordleGWindow.get_current_row(gw))
 
 
     gw = WordleGWindow()
@@ -48,9 +54,9 @@ def wordle():
     #LOOP THROUGH AND PLACE LETTERS FROM WORD INTO FIRST ROW OF WORDLE
     colLoop = N_COLS
     rowLoop = N_ROWS
-    for x in word:
-        WordleGWindow.set_square_letter(gw, N_ROWS - rowLoop, N_COLS - colLoop, x)
-        colLoop = colLoop - 1
+    # for x in word:
+    #     WordleGWindow.set_square_letter(gw, N_ROWS - rowLoop, N_COLS - colLoop, x)
+    #     colLoop = colLoop - 1
 
     gw.add_enter_listener(enter_action)
 
